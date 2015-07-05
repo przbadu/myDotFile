17c2a7278bf6bd55f31ac1f3c96cc59208104d9d
Pushpa Raj Badu <przbadu.social@gmail.com>
2015-07-05 16:11:08 +0530

    fixed path issue. Also add path to vagrant directory

--- oh-my-zsh/prz/prz.plugin.zsh
+++ oh-my-zsh/prz/prz.plugin.zsh
@@ -1,3 +1,7 @@
-c() { cd /projects/$1; }
-_c() { _files -W /projects -/; }
+c() { cd ~/projects/$1; }
+_c() { _files -W ~/projects -/; }
 compdef _c c
+
+cv() { cd /projects/Vagrant/$1; }
+_cv() { _files -W ~/projects/Vagrant -/; }
+compdef _cv cv
